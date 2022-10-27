167e564bec85f0c689e3b748f1fecfbbb6038b03
Iryna <pysarenkoiryna@gmail.com>
2022-10-27 22:12:06 +0300

    update

--- src/components/TestCard/TestCard.jsx
+++ src/components/TestCard/TestCard.jsx
@@ -1,13 +1,24 @@
 import { useState } from 'react';
 import { useDispatch } from 'react-redux';
 
+function fun1() {
+  const ahsver = [];
+  const rad = document.getElementsByName('r1');
+  for (let i = 0; i < rad.length; i++) {
+    if (rad[i].checked) {
+      ahsver.push(rad[i].textContent);
+    }
+  }
+}
+
 const TestCard = () => {
   return (
     <>
       <p>Question?</p>
-      <input type="checkbox" />A
-      <input type="checkbox" />B
-      <input type="checkbox" />C
+      <input name="r1" type="radio" /> Первый
+      <input name="r1" type="radio" /> Второй
+      <input name="r1" type="radio" /> Третий
+      <button type="button"></button>
     </>
   );
 };
--- src/redux/tests/tests-slice.js
+++ src/redux/tests/tests-slice.js
@@ -1,3 +1,12 @@
 import { createSlice } from '@reduxjs/toolkit';
 import { theoryTest, techTest } from './tests-operations';
 import { testInitialState } from './test-initialState';
+
+
+const testSlice = createSlice({
+  name: 'test',
+  testInitialState,
+    extraReducers: {
+      
+  },
+}); 
