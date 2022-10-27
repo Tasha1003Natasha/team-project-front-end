465454e847c601b9877764986c2f387b4be58ae0
Sergey Shevchenko <98112335+serg-itsh@users.noreply.github.com>
2022-10-27 15:36:01 +0300

    Створю компонент main-page.

--- src/Pages/MainPage/MainPage.jsx
+++ src/Pages/MainPage/MainPage.jsx
@@ -1,7 +1,45 @@
-// import styles from './MainPage.module.css';
+// import { ReactComponent as Arrow } from '../../images/icons/icons.svg#arrow';
+import Sprite from '../../images/icons/icons.svg';
+
+import styles from './MainPage.module.css';
 
 const MainPage = () => {
-  return <></>;
+  return (
+    <main>
+      <section className={styles.mainSection}>
+        <div className={styles.container}>
+          <div className={styles.mainWrapper}>
+            <div className={styles.descriptionWrappper}>
+              <h1 className={styles.mainTitle}>
+              “Regression testing. What is it? <br/> If the system compiles, that's good, if it boots, that's great!”
+              </h1>
+              <hr className={styles.mainLine}/>
+              
+              <p className={styles.mainTextStrong}>Linus Torvalds</p>
+              
+              <p className={styles.mainText}>Linux kernel creator, hacker, 1969</p>
+            </div>
+            <div className={styles.btnsWrapper}>
+              <button className={styles.mainButton}>QA technical training
+              
+                <svg className={styles.arrowIcon} width={24} height={16}>
+              <use href={`${Sprite}#arrow`}></use>
+            </svg>
+
+
+                </button>
+              <button className={styles.mainButtonTest}>Testing theory
+              <svg className={styles.arrowIcon} width={24} height={16}>
+              <use href={`${Sprite}#arrow`}></use>
+            </svg>
+                </button>
+            </div>
+            
+          </div>
+        </div>
+      </section>
+    </main>
+  );
 };
 
 export default MainPage;
