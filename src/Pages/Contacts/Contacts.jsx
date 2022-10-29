b6198eae3b2544332e91ffc5854fa2295a5c7bc4
Sergey Shevchenko <98112335+serg-itsh@users.noreply.github.com>
2022-10-29 12:35:57 +0300

    Created contacts komponent

---
+++ src/Pages/Contacts/Contacts.jsx
@@ -0,0 +1,344 @@
+import Sprite from '../../images/icons/icons-social.svg';
+import styles from './Contacts.module.css';
+
+import Natasha from '../../images/team/Natasha.png';
+import Serhii from '../../images/team/Serhii.jpg';
+import Ira from '../../images/team/Ira.jpg';
+import Vova from '../../images/team/Vova.jpg';
+import Roma from '../../images/team/Roma.jpg';
+import Serhii_O from '../../images/team/Serhii_O.jpg';
+import Sanya from '../../images/team/Sanya.jpg';
+
+
+const Contacts = () => {
+  return (
+    <main>
+      <section className={styles.section}>
+        <div className={styles.container}>
+          <h1 className={styles.title}>Our team</h1>
+          <p><hr className={styles.line}/></p>
+{/* Natasha */}
+          <ul className={styles.card__wrapper}>
+            <li className={styles.card}> 
+
+              <img
+                  src={Natasha}
+                  alt="1"
+                  className={styles.img}
+                  width="280"
+                  height="244"
+                />
+
+              <div className={styles.team__about}>
+                <h3 className={styles.member__title}>Natasha</h3>
+                <p className={styles.role__text}>Team Lead</p>
+
+                <ul className={styles.social__list}>
+                  <li className={styles.list__item}>
+                    <a
+                      className={styles.social__link}
+                      href="https://github.com/Tasha1003Natasha"
+                      target="blank"
+                    >                          
+                      <svg className={styles.arrowIcon} width={24} height={24}>
+                        <use href={`${Sprite}#icon-github-1`}></use>
+                      </svg>
+                    </a>
+                  </li>
+                  <li className={styles.list__item}>
+                    <a
+                      className={styles.social__link}
+                      href="https://www.linkedin.com/in/"
+                      target="blank"
+                    >
+                      
+                      <svg className={styles.arrowIcon} width={24} height={24}>
+                        <use href={`${Sprite}#icon-linkedin-1`}></use>
+                      </svg>
+                    </a>
+                  </li>                      
+                  
+                </ul>
+              </div>
+            </li>
+          {/* </ul> */}
+{/* Serhii */}
+          {/* <ul className={styles.card__wrapper}> */}
+            <li className={styles.card}> 
+
+              <img
+                  src={Serhii}
+                  alt="1"
+                  className={styles.img}
+                  width="280"
+                  height="244"
+                />
+
+              <div className={styles.team__about}>
+                <h3 className={styles.member__title}>Serhii</h3>
+                <p className={styles.role__text}>Scrum Master</p>
+
+                <ul className={styles.social__list}>
+                  <li className={styles.list__item}>
+                    <a
+                      className={styles.social__link}
+                      href="https://github.com/serg-itsh"
+                      target="blank"
+                    >                          
+                      <svg className={styles.arrowIcon} width={24} height={24}>
+                        <use href={`${Sprite}#icon-github-1`}></use>
+                      </svg>
+                    </a>
+                  </li>
+                  <li className={styles.list__item}>
+                    <a
+                      className={styles.social__link}
+                      href="https://www.linkedin.com/in/serhii-shevchenko-9889a3244/"
+                      target="blank"
+                    >
+                      
+                      <svg className={styles.arrowIcon} width={24} height={24}>
+                        <use href={`${Sprite}#icon-linkedin-1`}></use>
+                      </svg>
+                    </a>
+                  </li>                      
+                  
+                </ul>
+              </div>
+            </li>
+          {/* </ul> */}
+{/* Ira */}
+          {/* <ul className={styles.card__wrapper}> */}
+            <li className={styles.card}> 
+
+              <img
+                  src={Ira}
+                  alt="1"
+                  className={styles.img}
+                  width="280"
+                  height="244"
+                />
+
+              <div className={styles.team__about}>
+                <h3 className={styles.member__title}>Iryna</h3>
+                <p className={styles.role__text}>Developer</p>
+
+                <ul className={styles.social__list}>
+                  <li className={styles.list__item}>
+                    <a
+                      className={styles.social__link}
+                      href="https://github.com/IrynaSabinina"
+                      target="blank"
+                    >                          
+                      <svg className={styles.arrowIcon} width={24} height={24}>
+                        <use href={`${Sprite}#icon-github-1`}></use>
+                      </svg>
+                    </a>
+                  </li>
+                  <li className={styles.list__item}>
+                    <a
+                      className={styles.social__link}
+                      href="https://www.linkedin.com/in/iryna-sabinina-7620a5244/"
+                      target="blank"
+                    >
+                      
+                      <svg className={styles.arrowIcon} width={24} height={24}>
+                        <use href={`${Sprite}#icon-linkedin-1`}></use>
+                      </svg>
+                    </a>
+                  </li>                      
+                  
+                </ul>
+              </div>
+            </li>
+          {/* </ul> */}
+{/* Vova */}
+          {/* <ul className={styles.card__wrapper}> */}
+            <li className={styles.card}> 
+
+              <img
+                  src={Vova}
+                  alt="1"
+                  className={styles.img}
+                  width="280"
+                  height="244"
+                />
+
+              <div className={styles.team__about}>
+                <h3 className={styles.member__title}>Volodymyr</h3>
+                <p className={styles.role__text}>Developer</p>
+
+                <ul className={styles.social__list}>
+                  <li className={styles.list__item}>
+                    <a
+                      className={styles.social__link}
+                      href="https://github.com/Mr-Nihility"
+                      target="blank"
+                    >                          
+                      <svg className={styles.arrowIcon} width={24} height={24}>
+                        <use href={`${Sprite}#icon-github-1`}></use>
+                      </svg>
+                    </a>
+                  </li>
+                  <li className={styles.list__item}>
+                    <a
+                      className={styles.social__link}
+                      href="https://www.linkedin.com/in/volodymyr-romanenko-b0b15b243/"
+                      target="blank"
+                    >
+                      
+                      <svg className={styles.arrowIcon} width={24} height={24}>
+                        <use href={`${Sprite}#icon-linkedin-1`}></use>
+                      </svg>
+                    </a>
+                  </li>                      
+                  
+                </ul>
+              </div>
+            </li>
+          {/* </ul> */}
+{/* Roma */}
+          {/* <ul className={styles.card__wrapper}> */}
+            <li className={styles.card}> 
+
+              <img
+                  src={Roma}
+                  alt="1"
+                  className={styles.img}
+                  width="280"
+                  height="244"
+                />
+
+              <div className={styles.team__about}>
+                <h3 className={styles.member__title}>Roman</h3>
+                <p className={styles.role__text}>Developer</p>
+
+                <ul className={styles.social__list}>
+                  <li className={styles.list__item}>
+                    <a
+                      className={styles.social__link}
+                      href="https://github.com/RoRomario360"
+                      target="blank"
+                    >                          
+                      <svg className={styles.arrowIcon} width={24} height={24}>
+                        <use href={`${Sprite}#icon-github-1`}></use>
+                      </svg>
+                    </a>
+                  </li>
+                  <li className={styles.list__item}>
+                    <a
+                      className={styles.social__link}
+                      href="https://www.linkedin.com/in/roman-merkulov-857041241/"
+                      target="blank"
+                    >
+                      
+                      <svg className={styles.arrowIcon} width={24} height={24}>
+                        <use href={`${Sprite}#icon-linkedin-1`}></use>
+                      </svg>
+                    </a>
+                  </li>                      
+                  
+                </ul>
+              </div>
+            </li>
+          {/* </ul> */}
+{/* Serhii_O*/}
+          {/* <ul className={styles.card__wrapper}> */}
+            <li className={styles.card}> 
+
+              <img
+                  src={Serhii_O}
+                  alt="1"
+                  className={styles.img}
+                  width="280"
+                  height="244"
+                />
+
+              <div className={styles.team__about}>
+                <h3 className={styles.member__title}>Serhii</h3>
+                <p className={styles.role__text}>Developer</p>
+
+                <ul className={styles.social__list}>
+                  <li className={styles.list__item}>
+                    <a
+                      className={styles.social__link}
+                      href="https://github.com/SergeyOstapenko"
+                      target="blank"
+                    >                          
+                      <svg className={styles.arrowIcon} width={24} height={24}>
+                        <use href={`${Sprite}#icon-github-1`}></use>
+                      </svg>
+                    </a>
+                  </li>
+                  <li className={styles.list__item}>
+                    <a
+                      className={styles.social__link}
+                      href="https://www.linkedin.com/in/"
+                      target="blank"
+                    >
+                      
+                      <svg className={styles.arrowIcon} width={24} height={24}>
+                        <use href={`${Sprite}#icon-linkedin-1`}></use>
+                      </svg>
+                    </a>
+                  </li>                      
+                  
+                </ul>
+              </div>
+            </li>
+          {/* </ul> */}
+{/* Sanya */}
+          {/* <ul className={styles.card__wrapper}> */}
+            <li className={styles.card}> 
+
+              <img
+                  src={Sanya}
+                  alt="1"
+                  className={styles.img}
+                  width="280"
+                  height="244"
+                />
+
+              <div className={styles.team__about}>
+                <h3 className={styles.member__title}>Alexandr</h3>
+                <p className={styles.role__text}>Developer</p>
+
+                <ul className={styles.social__list}>
+                  <li className={styles.list__item}>
+                    <a
+                      className={styles.social__link}
+                      href="https://github.com/Sinbew"
+                      target="blank"
+                    >                          
+                      <svg className={styles.arrowIcon} width={24} height={24}>
+                        <use href={`${Sprite}#icon-github-1`}></use>
+                      </svg>
+                    </a>
+                  </li>
+                  <li className={styles.list__item}>
+                    <a
+                      className={styles.social__link}
+                      href="https://www.linkedin.com/in/alexandr-raskulov/"
+                      target="blank"
+                    >
+                      
+                      <svg className={styles.arrowIcon} width={24} height={24}>
+                        <use href={`${Sprite}#icon-linkedin-1`}></use>
+                      </svg>
+                    </a>
+                  </li>                      
+                  
+                </ul>
+              </div>
+            </li>
+          </ul>
+
+          
+
+        </div>
+      </section>
+    </main>
+  );
+};
+
+export default Contacts;
