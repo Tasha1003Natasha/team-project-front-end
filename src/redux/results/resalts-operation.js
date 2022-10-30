5126317d16bf2a61aceab1fe413c1682877c0d0e
Iryna <pysarenkoiryna@gmail.com>
2022-10-30 16:13:06 +0200

    update

--- src/redux/results/resalts-operation.js
+++ src/redux/results/resalts-operation.js
@@ -2,12 +2,12 @@ import { createAsyncThunk } from '@reduxjs/toolkit';
 import { API, tokenAuth } from 'API';
 import { toast } from 'react-toastify';
 
-export const results = createAsyncThunk('/results', async answers => {
+export const results = createAsyncThunk('/tests/results', async answers => {
   if (!tokenAuth) {
     toast.error('Login please, first');
   }
 
-  const { data } = await API.post('/results', answers);
+  const { data } = await API.post('/tests/results', answers);
   console.log(data);
   return data;
 
