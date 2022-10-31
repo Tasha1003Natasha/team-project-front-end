84da8ba735222576f483a06fedd6315136a2ec7e
RoRomario360 <fotoromario@gmail.com>
2022-10-31 12:33:01 +0200

    loader_chart

--- src/Pages/PageNotFound/PageNotFound.jsx
+++ src/Pages/PageNotFound/PageNotFound.jsx
@@ -1,7 +1,15 @@
-// import styles from '../PageNotFound/PageNotFound.module.css';
+import s from '../PageNotFound/PageNotFound.module.css';
 
 const PageNotFound = () => {
-  return <></>;
+  <div className={s.pageWrapper}>
+    <div className={s.gifBox}>
+      <img
+        src="https://tenor.com/view/not-found-404error-crash-glitch-gif-23664505"
+        alt="gif"
+      />
+      <h1>Test page</h1>
+    </div>
+  </div>;
 };
 
 export default PageNotFound;
