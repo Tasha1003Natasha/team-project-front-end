d2e6684c098f7808c677bd767c1270a8cc3069f5
SergeyOstapenko <sergiy.v.ostapenko@gmail.com>
2022-11-03 10:39:29 +0200

    com5

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -11,7 +11,7 @@ const Navigation = () => {
   return (
     <>
       <header className={styles.header}>
-        <div className={styles.mobileWrapper}>
+        {/* <div className={styles.mobileWrapper}>
           <div className={styles.container}>
             <div className={styles.header__nav}>
               <Link to="/">
@@ -27,9 +27,9 @@ const Navigation = () => {
               </div>
             </div>
           </div>
-        </div>
+        </div> */}
         {''}
-        <div className={styles.desktopWrapper}>
+        {/* <div className={styles.desktopWrapper}>
           <div className={styles.container}>
             <div className={styles.header__nav}>
               <Link to="/">
@@ -52,8 +52,45 @@ const Navigation = () => {
             </div>
             {isLogin && <UserMenu />}
           </div>
+        </div> */}
+
+        {/* MenuMobileWrapper */}
+        <div className={styles.mobileWrapper}>
+          <div className={styles.container}>
+            <div className={styles.header__nav}>
+              <Link to="/">
+                <svg width={129} height={28} className={styles.header__logo}>
+                  <use href={`${Sprite}#logo-header`}></use>
+                </svg>
+              </Link>
+              <div className={styles.menu__container}>
+                <span className={styles.line}></span>
+                <svg width={26} height={26}>
+                  <use href={`${Sprite}#close`}></use>
+                </svg>
+              </div>
+            </div>
+          </div>
         </div>
+
+        {/* MenuDesktopWrapper */}
       </header>
+      <div className={styles.menu}>
+        <ul>
+          <li className={styles.menu_item}>
+            <NavLink
+              to="/contacts"
+              className={styles.menu__list_item_text}
+            >
+              Contacts
+            </NavLink>
+          </li>
+        </ul>
+      </div>
+
+
+
+      
     </>
   );
 };
