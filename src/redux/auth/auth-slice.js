0309c12bce0b6f76775616f4313a5af080042d70
Tasha1003Natasha <0955712824n@gmail.com>
2022-11-07 13:11:12 +0200

    userCurrent правка

--- src/redux/auth/auth-slice.js
+++ src/redux/auth/auth-slice.js
@@ -67,9 +67,11 @@ const authSlice = createSlice({
     [userCurrent.fulfilled]: (state, { payload }) => {
       state.loading = false;
       state.isLogin = true;
+
+      state.user = payload.user;
       // state.token = payload.token;
       // state.user.email = payload.user.email;
-      state.user = payload;
+      // state.user = payload;
     },
     [userCurrent.rejected]: (state, { payload }) => {
       state.loading = false;
